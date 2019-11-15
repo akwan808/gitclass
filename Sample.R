ed8b054c1e668c6b9670a25302051c6920d31548
Abraham Kwan <akwan808@gwu.edu>
2019-11-15 10:33:26 -0500

    Added comments and steps

--- Sample.R
+++ Sample.R
@@ -1,3 +1,11 @@
+#Sample Code
 alpha <- 0.05
 age <- c(18,25,35,40,22,30,100)
 hist(age)
+
+#Save file
+#Click git on the top right box
+#Check off the files: N (new), M (modify), D(delete)
+#Commit
+#Commit comment
+#Push --> Github folder with changes marked
